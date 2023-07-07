2cf4719b662854674f2f7337f77f4d54853d1810
alwstar <60684855+alwstar@users.noreply.github.com>
2023-07-07 17:34:52 +0200

    adjusted ETL process to load to DB

--- SQL/Create Table stock_data_forecast.sql
+++ SQL/Create Table stock_data_forecast.sql
@@ -1,12 +1,12 @@
 CREATE TABLE stock_data_forecast (
     id INT IDENTITY(1,1) PRIMARY KEY,
-    timestamp DATETIME,
+    [date] DATETIME,
 	forecast_date DATETIME,
+	company VARCHAR(255),
 	[type] VARCHAR(255),
     [open] FLOAT,
     high FLOAT,
     low FLOAT,
     [close] FLOAT,
-    volume BIGINT,
-    company VARCHAR(255)
+    volume BIGINT
 )
