e2bf2e2f8d8afc3f66a5e82f1b73fd2655700927
alwstar <alwis.stark@web.de>
2023-08-30 10:28:24 +0200

    folder structure cleaned up

--- SQL/archieved/Table for stock_data.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE stock_data (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    timestamp DATETIME,
-    [open] FLOAT,
-    high FLOAT,
-    low FLOAT,
-    [close] FLOAT,
-    volume BIGINT
-)
